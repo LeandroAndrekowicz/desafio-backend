828869ecb00740ef921d2ca06f6511c8552faa15
Leandro Andrekowicz <103007824+LeandroAndrekowicz@users.noreply.github.com>
2025-08-24 13:06:29 -0300

    Configuring SWAGGER

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,14 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Desafio backend - SWAGGER')
+    .setDescription('SWAGGER de documentação das rotas do backend')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   app.setGlobalPrefix('api');
   app.enableCors();
